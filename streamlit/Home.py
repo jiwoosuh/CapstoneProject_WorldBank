63d9d5578ee75fef4ee27760dd23171b907e751c
BrooklynChen <brooklynchen@gwu.edu>
2024-04-04 16:42:56 -0400

    updated Folder Upload in page

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -15,10 +15,10 @@ from streamlit.components.v1 import components
 from source_code.word2csv import get_file_locations, extract_info_from_docx, convert_table_to_csv_file
 # from source_code.geo_viz import init_map, create_point_map, plot_from_df, load_df, load_map_region, load_map_state, main_region, main_state, plots
 from source_code.data_cleaning import clean_date_format, fix_year_format, clean_mem_status, clean_transaction_amount
-from source_code.pdf2csv_easyOCR import ocr_result
+# from source_code.pdf2csv_easyOCR import ocr_result
 
 # st.title('Welcome!')
-st.title('Welcome to :blue[Doc2Dashboard]🪄')
+st.title('Welcome to :blue[Docx2Dashboard]🪄')
 st.header('Introducing Our Solution for Digital Transformation', divider='grey')
 st.subheader("💁‍♀️ Empowering unbanked Nigerian women through Digital Transformation.")
 st.subheader("📑 Convert paper-based financial records into CSV files effortlessly.")
